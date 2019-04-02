76d804c8b4554c32adb4fff6eae464cb7c7e5464
Pascal Allen <pascal@onlinemeded.org>
2019-04-02 15:26:25 -0500

    init

---
+++ database/migrations/2019_04_02_202345_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+            $table->string('imgur_id');
+            $table->integer('category_id')->nullable();
+            $table->string('slug');
+            $table->boolean('main')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
